0b64bf572d20626feeadd9e07beab80655f6d7a5
EC2 Default User <ec2-user@ip-172-31-7-247.ap-northeast-1.compute.internal>
2023-02-05 07:54:00 +0000

    ストップウォッチ

---
+++ main.js
@@ -0,0 +1,76 @@
+let showTime;
+// 初めの時間
+let startTime;
+// 経過時間
+let elapsedTime = 0 ;
+//一時停止時間
+let holdTime = 0;
+
+let timeId;
+
+// window.onload = function(){
+const timer = document.getElementById('timer');
+const startbtn = document.getElementById('start');
+const stopbtn = document.getElementById('stop');
+const resetbtn = document.getElementById('reset');
+
+function updateTimeText(){
+    let ma = Math.floor(elapsedTime / 1000000);
+    let mb = Math.floor(elapsedTime / 100000);
+    let m = Math.floor(elapsedTime / 10000);
+    let s = Math.floor(elapsedTime % 10000 / 1000);
+    let ms = elapsedTime %1000;
+   
+    ma = ('0' + ma).slice(-1);
+    mb = ('0' + mb).slice(-1);
+    m = ('0' + m).slice(-1);
+    s = ('0' + s).slice(-1);
+   
+    timer.textContent = ma+':'+mb+':'+m+':'+s;
+    
+  
+}
+
+startbtn.addEventListener("click",function(){
+ 
+  startTime = Date.now();
+  countUp();
+  
+  startbtn.disabled = true;
+  stopbtn.disabled = false;
+  resetbtn.disabled = false; 
+  
+})
+
+stopbtn.addEventListener("click",function(){
+  
+  clearTimeout(timeId);
+  
+  startbtn.disabled = false;
+  stopbtn.disabled = true;
+  resetbtn.disabled = false;   
+  
+  holdTime += Date.now() - startTime;
+  
+})
+
+resetbtn.addEventListener("click",function(){
+  elapsedTime = 0 ;
+  holdTime = 0;
+  updateTimeText();
+  
+  startbtn.disabled = false;
+  stopbtn.disabled = false;
+  resetbtn.disabled = true;  
+})
+
+  
+function countUp(){
+   
+  timeId = setTimeout(function(){
+    elapsedTime = Date.now() -startTime + holdTime;
+    updateTimeText();
+    countUp();
+     
+  },10);
+}
